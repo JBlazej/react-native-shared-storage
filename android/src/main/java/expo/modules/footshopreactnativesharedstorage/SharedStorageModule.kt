c6e44560856c0eba9a53431d38d145fd6e83c9b3
Jan Blazej <jan.blazej@footshop.cz>
2023-08-16 11:20:19 +0200

    Android methods basic implementation

--- android/src/main/java/expo/modules/footshopreactnativesharedstorage/SharedStorageModule.kt
+++ android/src/main/java/expo/modules/footshopreactnativesharedstorage/SharedStorageModule.kt
@@ -1,31 +1,60 @@
 package expo.modules.footshopreactnativesharedstorage
 
+import android.content.Context
+import android.content.SharedPreferences
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.modules.Promise
 
 class SharedStorageModule : Module() {
+  private lateinit var sharedPreferences: SharedPreferences
+
+  init {
+    sharedPreferences = context.getSharedPreferences("SharedStorageModule", Context.MODE_PRIVATE)
+  }
+
   override fun definition() = ModuleDefinition {
     Name("SharedStorage")
 
-    // TODO implement methods
-    AsyncFunction("get") { message: String, promise: Promise ->
-      launch(Dispatchers.Main) {
-        promise.resolve(message)
-      }
+    AsyncFunction("get") { key: String, promise: Promise ->
+        launch(Dispatchers.Main) {
+            try {
+                val value = sharedPreferences.getString(key, null)
+                promise.resolve(value)
+            } catch (e: Exception) {
+                promise.reject("GET_ERROR", "An error occurred while retrieving data.", e)
+            }
+        }
     }
 
-    // TODO implement methods
-    AsyncFunction("set") { message: String, promise: Promise ->
-      launch(Dispatchers.Main) {
-        promise.resolve(message)
-      }
+    AsyncFunction("set") { params: Map<String, String>, promise: Promise ->
+        launch(Dispatchers.Main) {
+            try {
+                val editor = sharedPreferences.edit()
+                params.forEach { (key, value) ->
+                    editor.putString(key, value)
+                }
+                editor.apply()
+                promise.resolve(null)
+            } catch (e: Exception) {
+                promise.reject("SET_ERROR", "An error occurred while saving data.", e)
+            }
+        }
     }
 
-    // TODO implement methods
-    AsyncFunction("remove") { message: String, promise: Promise ->
-      launch(Dispatchers.Main) {
-        promise.resolve(message)
-      }
+    AsyncFunction("remove") { key: String, promise: Promise ->
+        launch(Dispatchers.Main) {
+            try {
+                val editor = sharedPreferences.edit()
+                editor.remove(key)
+                editor.apply()
+                promise.resolve(null)
+            } catch (e: Exception) {
+                promise.reject("REMOVE_ERROR", "An error occurred while removing data.", e)
+            }
+        }
     }
   }
 }
